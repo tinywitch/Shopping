c91567b671a2340b3ff76b0fac746e486eb9c853
Tran B.V. Son <tranbavinhson@gmail.com>
2016-10-15 00:51:11 +0700

    Init Project and add gem devise

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_paramters, if: :devise_controller?
+
+  protected
+    def configure_permitted_paramters
+      devise_parameter_sanitizer.for(:sign_up) << :fullname
+      devise_parameter_sanitizer.for(:account_update) << :fullname
+    end
+end
